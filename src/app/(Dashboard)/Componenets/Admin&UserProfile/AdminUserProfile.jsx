959b22d61f800da5975393e20bdbe0d6a5f81e94
unknown <prantokih42@gmail.com>
2025-04-28 22:37:05 +0600

    profile loading added

--- src/app/(Dashboard)/Componenets/Admin&UserProfile/AdminUserProfile.jsx
+++ src/app/(Dashboard)/Componenets/Admin&UserProfile/AdminUserProfile.jsx
@@ -3,9 +3,14 @@ import AdminGuard from "@/src/services/AdminGuard";
 import AdminProfile from "../../Componenets/AdminProfile/AdminProfile";
 import CustomerProfile from "../../Componenets/CustomerProfile/CustomerProfile";
 import { useSession } from "next-auth/react";
+import Loading from "@/src/Loading";
 const AdminUserProfile = () => {
     const { data: session, status } = useSession();
     const role = session?.user?.role;
+    if (status === "loading") {
+        return <div><Loading></Loading></div>; 
+    }
+
     return (
         <div>
             {role === "admin" ? (
